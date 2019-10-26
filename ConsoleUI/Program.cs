32b126587eae203d676a68cad66664fb339d0f59
david colvin <davidmcolvin@gmail.com>
2019-10-25 22:16:41 -0400

    Added advanced filters and conversion pattern examples

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,12 +10,34 @@ namespace ConsoleUI
 {
   class Program
   {
-    private static readonly log4net.ILog log = LogHelper.GetLogger(); // log4net.LogManager.GetLogger( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType );
-    static void Main(string[] args)
+    private static readonly log4net.ILog log = log4net.LogManager.GetLogger( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType ); //LogHelper.GetLogger();
+    static void Main(string[] args)//
     {
-      Console.WriteLine("Hello World");
+      log.Debug("Developer: Tutorial was run");
+      log.Info("Maintenance: water pump turned on");
+      log.Warn("Maintenance: the water pump is getting hot");
 
-      log.Error("This is my error message");
+      var i = 0;
+
+      try
+      {
+        var x = 10 / i;
+      }
+      catch (DivideByZeroException ex)
+      {
+        log.Error("Developer: we tried to divide by zero");
+      }
+
+      Counter j = new Counter();
+
+      log4net.GlobalContext.Properties["Counter"] = j;
+
+      for (j.LoopCounter = 0; j.LoopCounter < 5; j.LoopCounter++)
+      {
+        log.Fatal("This is a fatal error in the process");
+      }
+      
+      /*log.Fatal("Maintenance: water pump exploded")*/
 
       Console.ReadLine();
 
